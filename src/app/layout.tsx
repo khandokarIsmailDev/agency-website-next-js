7bbf810dfc0a018e55ff6cad4fb7f6cbd73d73fc
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2023-12-03 14:18:44 +0600

    add meta data propertise

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,20 @@ import RootProvider from "@/components/RootProvider.";
 import Footer from "@/components/Footer";
 
 
+
+export async function generateMetadata(){
+    const res=await fetch(process.env.BASE_URL+"api/SiteMeta/home");
+    const JSON=await res.json();
+    return{
+        title:JSON[0]['title'],
+        description:JSON[0]['description'],
+        keywords:JSON[0]['keywords'],
+        openGraph: {
+            images: JSON[0]['image'],
+        },
+    }
+}
+
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
--- src/app/team/layout.tsx
+++ src/app/team/layout.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
-export default function RootLayout({children} : {children:React.ReactNode}){
+
+export async function generateMetadata(){
+    const res=await fetch(process.env.BASE_URL+"api/SiteMeta/team");
+    const JSON=await res.json();
+    return{
+        title:JSON[0]['title'],
+        description:JSON[0]['description'],
+        keywords:JSON[0]['keywords'],
+        openGraph: {
+            images: JSON[0]['image'],
+        },
+    }
+}
+export default function RootLayout({children} ){
     return(
         <div>
             {children}
